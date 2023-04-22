941fcb7f2aef794dd72b1a13ce3d3c6cfa3179ed
r0binov <robinpall91@gmail.com>
2023-04-23 01:41:02 +0300

    feat(frontend): initial commit

--- pages/extension/content.js
+++ pages/extension/content.js
@@ -1,9 +1,44 @@
-console.log('brrr')
+const apiKey = "";
+const completionsEndpoint = 'https://api.openai.com/v1/completions';
 
-function popUpWindow() {
-    console.log('popUpWindow');
-//KUVA LAADIMISMÄRK JA ALUSTA JÄRGMISE FUNKTSIOONIGA MIS PANEB EMAILID PÕHJAL SENDERI KONTROLLI
+const submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', () => {
+  const inputText = document.getElementById('inputText').value;
+  generateCompletion(apiKey, completionsEndpoint, inputText);
+});
 
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+function generateCompletion(apiKey, completionsEndpoint, inputText) {
+  const data = JSON.stringify({
+    prompt: `Scan the text or website ${inputText} and provide a summary of how suspicious it appears:`,
+    max_tokens: 50,
+    model: "text-davinci-003",
+    temperature: 0.5
+  });
+
+  const httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        const response = JSON.parse(httpRequest.responseText);
+
+        if (response.hasOwnProperty('choices') && response.choices.length > 0) {
+          const output = document.getElementById('output');
+          output.innerText = response.choices[0].text;
+        } else {
+          console.error(`Error generating completion: ${response.error.message}`);
+        }
+      } else {
+        console.error(`Error sending HTTP request: ${httpRequest.statusText}`);
+      }
+    }
+  };
+
+  httpRequest.open('POST', completionsEndpoint);
+  httpRequest.setRequestHeader('Content-Type', 'application/json');
+  httpRequest.setRequestHeader('Content-Length', data.length);
+  httpRequest.setRequestHeader('Authorization', `Bearer ${apiKey}`);
+
+  httpRequest.send(data);
 }
+
