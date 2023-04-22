67d1106f514f32218dc550b5aef985e3d0d084ee
r0binov <robinpall91@gmail.com>
2023-04-22 14:06:15 +0300

    initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -51,12 +51,5 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `Generate a score between 1 and 10 on how trustworthy is this text`;
 }
